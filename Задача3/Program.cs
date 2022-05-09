ae94761007c1e5b4399c4646887a88bbef3d72c6
Alex <lrl.msk@yandex.ru>
2022-05-09 12:18:12 +0300

    add задача3 и 4

--- Задача3/Program.cs
+++ Задача3/Program.cs
@@ -4,6 +4,44 @@
 2 -> 10 
 */
 
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());  // преобразует в натуральное число
+int k;
+string result = " ";
+while (a > 0)
+{
+    k = a % 2;
+    result = k + result;
+    a = a / 2;
+}
+Console.WriteLine($"Вывод: {result}");
+
+
+
+//   =======    2-й способ  ============
+/*
+                 //Console.Write("Введите число: ");
+                //a = Int32.Parse(Console.ReadLine());
+
+Console.Clear();
+
+string mass(int a)
+{
+    int b;
+    string result = String.Empty;
+    while(a > 0)
+    {
+        b = a % 2;
+        a = a / 2;
+        result = b.ToString() + result; // соединяются числа в строку, по очереди - не сумируются
+    }
+    return result;
+}
+System.Console.WriteLine(mass(45));
+
+*/
 
 
 
--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* не используя рекрсию, выведите первые N чисел Фибоначчи.
+Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8   */
+
+
+/*
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());  // преобразует в натуральное число
+
+int a = 0;
+int b = 0;
+while (n > 0)
+{ 
+
+}
+*/
+Console.Clear();
+
+void Fib(int N)
+{
+    int x = 0, y = 1, z = 0;
+    if(N <= 0)return;  // если заданное число отриц или 0 то выходим - ничего не произойдет
+    System.Console.Write(("0 "));
+    if(N >= 2)
+    System.Console.Write(("1 "));
+    for (int i = 0; i < N-2; i++)
+    {
+        z = x + y;
+        System.Console.Write(z + " ");
+        x = y;
+        y = z;
+    }
+}
+Fib(5);
+
+
+

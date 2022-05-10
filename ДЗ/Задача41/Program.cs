9d92938d2b18972149e167392855d3ec1ea66ade
Alex <lrl.msk@yandex.ru>
2022-05-10 19:09:05 +0300

    add Folder Lesson_2

---
+++ ДЗ/Задача41/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Пользователь вводит с клавиатуры М чисел. Посчитать, сколько
+чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223 -> 3    */
+
+
+Console.Write("Введите числа через пробел): ");
+int[] mass = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Вы ввели количество чисел больше 0: {count}");
+
+
+/*
+Console.Write("Введите числа через пробел: ");
+
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int Numbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine($"Кол-во чисел больше 0 равно: {Numbers(array)}");
+
+*/
+
+
+
+// считывает со строки и преобразует в числа
+/*  string line = "1 2 3 4 5";
+
+string[]massString = line.Split();
+int[]mass = new int[massString.Length];
+
+for (int i = 0; i < mass.Length; i++)
+    mass[i] = Convert.ToInt32(massString[i]);
+for (int i = 0; i < mass.Length; i++)
+    System.Console.WriteLine(mass[i]);
+*/
---
+++ ДЗ/Задача43/Program.cs
@@ -0,0 +1,25 @@
+﻿/*    программа, которая найдет точку пересечения двух прямых, заданых
+уравнениями y = k1 * x + b1, y = k2 * x + b2; значения  b1, k1, b2, k2 
+задаются ползователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)   */
+
+Console.Clear();
+// y = k1 * x + b1;
+// y = k2 * x + b2;
+// x = (b2 - b1) / (k1 - k2);
+// y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+
+string X(double k1, double k2, double b1, double b2)
+{
+    string result = string.Empty;
+    double x = 0;
+    double y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+    result = $"({x}, {y})";
+    return result;
+}
+Console.WriteLine(X(5, 9, 2, 4));
+
+
+
